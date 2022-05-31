78bba1a0863a04fd9045e7793f4b57aea4ccd5eb
andrii1 <andrii.gorokhovskyi@gmail.com>
2022-05-31 18:15:49 +0200

    if statements refactoring

--- javascript/javascript1/week2/homework.js
+++ javascript/javascript1/week2/homework.js
@@ -1,43 +1,41 @@
 // FLIGHT BOOKING
 
 function getFullname(firstname, lastname, useFormalName, gender) {
-	if (
-		typeof firstname === "undefined" ||
-		typeof lastname === "undefined" ||
-		firstname === "" ||
-		lastname === "" ||
-		typeof firstname !== "string" ||
-		typeof lastname !== "string"
-	) {
-		console.log("Please provide correct values");
-	} else {
-		if (useFormalName) {
-			if (gender === "woman") {
-				console.log(`Lady ${firstname} ${lastname}`);
-			} else if (gender === "man") {
-				console.log(`Lord ${firstname} ${lastname}`);
-			}
-		} else if (
-			gender !== "" &&
-			typeof gender !== "undefined" &&
-			gender !== "man" &&
-			gender !== "woman"
-		) {
-			console.log("Please add a gender");
-		} else {
-			console.log(`${firstname} ${lastname}`);
+	if (typeof firstname !== "string" || firstname === "") {
+		throw new Error("First name must be a non-empty string");
+	}
+	if (typeof lastname !== "string" || lastname === "") {
+		throw new Error("Last name must be a non-empty string");
+	}
+
+	if (useFormalName) {
+		if (gender === "woman") {
+			console.log(`Lady ${firstname} ${lastname}`);
+		} else if (gender === "man") {
+			console.log(`Lord ${firstname} ${lastname}`);
 		}
+	} else if (
+		gender !== "" &&
+		typeof gender !== "undefined" &&
+		gender !== "man" &&
+		gender !== "woman"
+	) {
+		console.log("Please add a gender");
+	} else {
+		console.log(`${firstname} ${lastname}`);
 	}
 }
 
 getFullname("Benjamin", "Hughes");
 getFullname("Andrii", "Gor");
 getFullname("Jessica", "Thompson", true, "woman");
-getFullname();
-getFullname("", "", true, "man");
-getFullname(true);
-getFullname(true, true, true);
-getFullname(true, "man");
+
+/* these statements throw errors */
+//getFullname();
+//getFullname("", "", true, "man");
+//getFullname(true);
+//getFullname(true, true, true);
+//getFullname(true, "man");
 
 //EVENT WEEKDAY
 
@@ -63,22 +61,22 @@ console.log(getEventWeekday(1));
 console.log(getEventWeekday(1));
 
 //WEATHER WEAR
-const clothes = [
-	["warm jacket", "warm pants", "scarf"],
-	["jacket", "pants", "sweater"],
-	["shorts", "shirt", "cap"],
-	["shorts", "white t-shirt", "cap", "bathing suit"],
-];
 
 function clothesGenerator(temp) {
+	const clothes = [
+		["warm jacket", "warm pants", "scarf"],
+		["jacket", "pants", "sweater"],
+		["shorts", "t-shirt"],
+		["shorts", "white t-shirt", "cap", "bathing suit"],
+	];
 	if (temp < 0) {
-		return clothes[0].join(", ");
-	} else if (temp >= 0 && temp < 10) {
-		return clothes[1].join(", ");
-	} else if (temp >= 10 && temp < 20) {
-		return clothes[2].join(", ");
+		return clothes[0].join(" and ");
+	} else if (temp < 10) {
+		return clothes[1].join(" and ");
+	} else if (temp < 20) {
+		return clothes[2].join(" and a ");
 	} else if (temp >= 20) {
-		return clothes[3].join(", ");
+		return clothes[3].join(" and ");
 	}
 }
 const clothesToWear = clothesGenerator(18);
@@ -89,18 +87,21 @@ const class07Students = [];
 let alreadyInClass;
 function addStudentToClass(studentName) {
 	// You write code here
-
-	if (class07Students.includes(studentName)) {
-		return `Student ${studentName} is already in the class`;
-	} else {
-		if (class07Students.length >= 6 && studentName != "Queen") {
-			return "Cannot add more students to class 07";
-		} else if (studentName === "") {
-			return "Cannot add an empty name";
+	if (typeof studentName === "string") {
+		if (class07Students.includes(studentName)) {
+			return `Student ${studentName} is already in the class`;
 		} else {
-			class07Students.push(studentName);
-			return `Student ${studentName} added to the class`;
+			if (class07Students.length >= 6 && studentName != "Queen") {
+				return "Cannot add more students to class 07";
+			} else if (studentName === "") {
+				return "Cannot add an empty name";
+			} else {
+				class07Students.push(studentName);
+				return `Student ${studentName} added to the class`;
+			}
 		}
+	} else {
+		return "Please enter a correct student name";
 	}
 }
 
@@ -109,6 +110,7 @@ function getNumberOfStudents() {
 	return class07Students.length;
 }
 
+console.log(addStudentToClass(true));
 console.log(addStudentToClass("Bob"));
 console.log(addStudentToClass("Bob"));
 console.log(addStudentToClass("Jessica"));
@@ -150,7 +152,6 @@ function canBuyMoreCandy(boughtCandyPrices, amountToSpend) {
 	for (let j = 0; j < boughtCandyPrices.length; j++) {
 		sum += boughtCandyPrices[j];
 	}
-
 	if (count >= amountToSpend) {
 		console.log("Enough candy for you!");
 	} else {
