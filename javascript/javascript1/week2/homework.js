85ec49c1c2c84093bd61fef75a1da10c1bd951ff
andrii1 <andrii.gorokhovskyi@gmail.com>
2022-05-31 13:34:18 +0200

    changes after review

--- javascript/javascript1/week2/homework.js
+++ javascript/javascript1/week2/homework.js
@@ -1,48 +1,85 @@
 // FLIGHT BOOKING
 
 function getFullname(firstname, lastname, useFormalName, gender) {
-  if (useFormalName) {
-    if (gender === "woman") {
-      return `Lady ${firstname} ${lastname}`
-    } else if (gender==="man") {
-      return `Lord ${firstname} ${lastname}`
-    }
-  } else {
-    return `${firstname} ${lastname}`
-  }
+	if (
+		typeof firstname === "undefined" ||
+		typeof lastname === "undefined" ||
+		firstname === "" ||
+		lastname === "" ||
+		typeof firstname !== "string" ||
+		typeof lastname !== "string"
+	) {
+		console.log("Please provide correct values");
+	} else {
+		if (useFormalName) {
+			if (gender === "woman") {
+				console.log(`Lady ${firstname} ${lastname}`);
+			} else if (gender === "man") {
+				console.log(`Lord ${firstname} ${lastname}`);
+			}
+		} else if (
+			gender !== "" &&
+			typeof gender !== "undefined" &&
+			gender !== "man" &&
+			gender !== "woman"
+		) {
+			console.log("Please add a gender");
+		} else {
+			console.log(`${firstname} ${lastname}`);
+		}
+	}
 }
 
 getFullname("Benjamin", "Hughes");
-const fullname1 = getFullname("Andrii", "Gor");
-const fullname2 = getFullname("Jessica", "Thompson", true, "woman");
-console.log(`${fullname1}, ${fullname2}`)
+getFullname("Andrii", "Gor");
+getFullname("Jessica", "Thompson", true, "woman");
+getFullname();
+getFullname("", "", true, "man");
+getFullname(true);
+getFullname(true, true, true);
+getFullname(true, "man");
 
 //EVENT WEEKDAY
 
-const date = new Date();
-const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 function getEventWeekday(days) {
-  daysAdded = date.setDate(date.getDate() + days);
-  futureDate = new Date(daysAdded)
-  dayNumber = futureDate.getDay()
-  dayOfWeek = weekday[dayNumber]
-  return dayOfWeek
+	const date = new Date();
+	const weekday = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+	const daysAdded = date.setDate(date.getDate() + days);
+	const futureDate = new Date(daysAdded);
+	const dayNumber = futureDate.getDay();
+	const dayOfWeek = weekday[dayNumber];
+	return dayOfWeek;
 }
-console.log(getEventWeekday(12))
+console.log(getEventWeekday(1));
+console.log(getEventWeekday(1));
+console.log(getEventWeekday(1));
 
 //WEATHER WEAR
-const clothes = [["warm jacket", "warm pants", "scarf"], ["jacket", "pants", "sweater"], ["shorts", "shirt", "cap"], ["shorts", "white t-shirt", "cap", "bathing suit"]];
+const clothes = [
+	["warm jacket", "warm pants", "scarf"],
+	["jacket", "pants", "sweater"],
+	["shorts", "shirt", "cap"],
+	["shorts", "white t-shirt", "cap", "bathing suit"],
+];
 
 function clothesGenerator(temp) {
-  if (temp < 0) {
-    return clothes[0].join(", ")
-  } else if ((temp >= 0) && (temp < 10)) {
-    return clothes[1].join(", ")
-  } else if ((temp >= 10) && (temp < 20)) {
-    return clothes[2].join(", ")
-  } else if (temp >= 20) {
-    return clothes[3].join(", ")
-  }
+	if (temp < 0) {
+		return clothes[0].join(", ");
+	} else if (temp >= 0 && temp < 10) {
+		return clothes[1].join(", ");
+	} else if (temp >= 10 && temp < 20) {
+		return clothes[2].join(", ");
+	} else if (temp >= 20) {
+		return clothes[3].join(", ");
+	}
 }
 const clothesToWear = clothesGenerator(18);
 console.log(clothesToWear); // Logs out: "shorts and a t-shirt"
@@ -51,82 +88,74 @@ console.log(clothesToWear); // Logs out: "shorts and a t-shirt"
 const class07Students = [];
 let alreadyInClass;
 function addStudentToClass(studentName) {
-  // You write code here
-  for (let i = 0; i <= class07Students.length; i++) {
-    if (studentName === class07Students[i]) {
-      return `Student ${studentName} is already in the class`
-      alreadyInClass = true;
-    }
-  }
-  if (!alreadyInClass) {
-    if ((class07Students.length >= 6) && (studentName != "Queen")) {
-      return "Cannot add more students to class 07";
-    } else if (studentName === "") {
-      return "Cannot add an empty name";
-    } else {
-      class07Students.push(studentName);
-      return `Student ${studentName} added to the class`
-    }
-  }
+	// You write code here
 
+	if (class07Students.includes(studentName)) {
+		return `Student ${studentName} is already in the class`;
+	} else {
+		if (class07Students.length >= 6 && studentName != "Queen") {
+			return "Cannot add more students to class 07";
+		} else if (studentName === "") {
+			return "Cannot add an empty name";
+		} else {
+			class07Students.push(studentName);
+			return `Student ${studentName} added to the class`;
+		}
+	}
 }
 
 function getNumberOfStudents() {
-  // You write code here
-  return class07Students.length;
+	// You write code here
+	return class07Students.length;
 }
 
-console.log(addStudentToClass("Bob"))
-console.log(addStudentToClass("Bob"))
-console.log(addStudentToClass("Jessica"))
-console.log(addStudentToClass("John"))
-console.log(addStudentToClass("Jessica"))
-console.log(addStudentToClass("Jessica"))
-console.log(addStudentToClass("Jack"))
-console.log(addStudentToClass("George"))
-console.log(addStudentToClass("Andrii"))
-console.log(addStudentToClass("Igor"))
-console.log(addStudentToClass("Queen"))
-console.log(getNumberOfStudents())
+console.log(addStudentToClass("Bob"));
+console.log(addStudentToClass("Bob"));
+console.log(addStudentToClass("Jessica"));
+console.log(addStudentToClass("John"));
+console.log(addStudentToClass("Jessica"));
+console.log(addStudentToClass("Jessica"));
+console.log(addStudentToClass("Jack"));
+console.log(addStudentToClass("George"));
+console.log(addStudentToClass("Andrii"));
+console.log(addStudentToClass("Igor"));
+console.log(addStudentToClass("Queen"));
+console.log(getNumberOfStudents());
 
 // CANDY HELPER
 const boughtCandyPrices = [];
 function addCandy(candyType, weight) {
-
-  switch (candyType) {
-  case 'Sweet':
-    boughtCandyPrices.push(weight*0.5)
-    break;
-  case 'Chocolate':
-    boughtCandyPrices.push(weight*0.7)
-    break;
-  case 'Toffee':
-    boughtCandyPrices.push(weight*1.1)
-    break;
-  case 'Chewing-gum':
-    boughtCandyPrices.push(weight*0.03)
-    break;
-}
-  return boughtCandyPrices
+	switch (candyType) {
+		case "Sweet":
+			boughtCandyPrices.push(weight * 0.5);
+			break;
+		case "Chocolate":
+			boughtCandyPrices.push(weight * 0.7);
+			break;
+		case "Toffee":
+			boughtCandyPrices.push(weight * 1.1);
+			break;
+		case "Chewing-gum":
+			boughtCandyPrices.push(weight * 0.03);
+			break;
+	}
+	return boughtCandyPrices;
 }
 addCandy("Toffee", 20);
 addCandy("Sweet", 10);
-const amountToSpend = Math.floor(Math.random() * 100)
+const amountToSpend = Math.floor(Math.random() * 100);
 
 function canBuyMoreCandy(boughtCandyPrices, amountToSpend) {
-  let count = 0;
-  for (let j=0; j<boughtCandyPrices.length; j++)
-   {
-    count += boughtCandyPrices[j];
-    console.log('test',boughtCandyPrices[j])
-   }
+	let sum = 0;
+	for (let j = 0; j < boughtCandyPrices.length; j++) {
+		sum += boughtCandyPrices[j];
+	}
 
-  if (count >= amountToSpend) {
-    console.log("Enough candy for you!")
-  } else {
-    console.log("You can buy more, so please do!")
-  }
-
+	if (count >= amountToSpend) {
+		console.log("Enough candy for you!");
+	} else {
+		console.log("You can buy more, so please do!");
+	}
 }
 
-canBuyMoreCandy(boughtCandyPrices, amountToSpend)
+canBuyMoreCandy(boughtCandyPrices, amountToSpend);
