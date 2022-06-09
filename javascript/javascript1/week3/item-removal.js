99a659cd3430bc6401741d159397e1dd48e6f572
andrii1 <andrii.gorokhovskyi@gmail.com>
2022-06-09 10:04:19 +0200

    first exercise

---
+++ javascript/javascript1/week3/item-removal.js
@@ -0,0 +1,20 @@
+const names = [
+	"Peter",
+	"Ahmad",
+	"Yana",
+	"kristina",
+	"Rasmus",
+	"Samuel",
+	"katrine",
+	"Tala",
+];
+const nameToRemove = "Ahmad";
+
+// Write some code here
+
+let index = names.indexOf(nameToRemove);
+names.splice(index, 1);
+
+// Code done
+
+console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
