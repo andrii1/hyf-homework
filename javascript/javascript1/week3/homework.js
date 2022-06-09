ed11f0535d4863ab16e43566590ccb8090c613e3
andrii1 <andrii.gorokhovskyi@gmail.com>
2022-06-09 14:27:33 +0200

    series duration

--- javascript/javascript1/week3/homework.js
+++ javascript/javascript1/week3/homework.js
@@ -56,3 +56,52 @@ const travelTime = calculateTravelTime(travelInformation);
 console.log(travelTime); // 8 hours and 38 minutes
 
 // SERIES DURATION OF MY LIFE
+
+const seriesDurations = [
+	{
+		title: "House of Cards",
+		days: 2,
+		hours: 12,
+		minutes: 50,
+	},
+	{
+		title: "Ozark",
+		days: 1,
+		hours: 17,
+		minutes: 4,
+	},
+	{
+		title: "Tiger King",
+		days: 0,
+		hours: 9,
+		minutes: 19,
+	},
+];
+
+/*seriesDurations.push({
+	title: "House of Cards",
+	days: 2,
+	hours: 16,
+	minutes: 29,
+});
+*/
+
+function logOutSeriesText(seriesDurations) {
+	// write code here
+	let sumPercentage = 0;
+	for (let i = 0; i < seriesDurations.length; i++) {
+		const timeInMinutes =
+			seriesDurations[i].days * 24 * 60 +
+			seriesDurations[i].hours * 60 +
+			seriesDurations[i].minutes * 60;
+		console.log(timeInMinutes);
+		const percentage = (timeInMinutes / (80 * 525600)) * 100;
+		console.log(
+			`${seriesDurations[i].title} took ${percentage.toFixed(4)} of my life`
+		);
+		sumPercentage += percentage;
+	}
+	console.log(`In total that is ${sumPercentage.toFixed(4)} of my life`);
+}
+
+logOutSeriesText(seriesDurations);
