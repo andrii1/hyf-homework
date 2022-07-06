ca4a203a54a304026e4c68509276734380345b34
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2022-07-06 14:27:55 +0200

    warmup excercise 1

---
+++ javascript/javascript2/week3/app.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(logAfterTwoSeconds, 2500);
+});
+function logAfterTwoSeconds() {
+  console.log("Called after 2.5 seconds");
+}
