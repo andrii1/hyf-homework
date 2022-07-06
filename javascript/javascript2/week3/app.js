8940046ada299f6f94362a13008b7612d0af78a3
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2022-07-06 14:43:06 +0200

    warmup 1.3

--- javascript/javascript2/week3/app.js
+++ javascript/javascript2/week3/app.js
@@ -1,6 +1,19 @@
+const button = document.querySelector("button");
 document.addEventListener("DOMContentLoaded", function () {
   setTimeout(logAfterTwoSeconds, 2500);
 });
 function logAfterTwoSeconds() {
   console.log("Called after 2.5 seconds");
 }
+function logTextAfterSpecificTime(delay, stringToLog) {
+  const delayMs = delay * 1000;
+  setTimeout(function () {
+    console.log(stringToLog);
+  }, delayMs);
+}
+
+logTextAfterSpecificTime(5, "log this text after 5 seconds");
+
+button.addEventListener("click", function () {
+  logTextAfterSpecificTime(5, "called after 5 sec");
+});
