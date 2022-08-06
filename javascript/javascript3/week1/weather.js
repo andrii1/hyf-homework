939f62fdb5a09a24f0c9851a829716e86149a5e9
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2022-08-06 16:49:17 +0200

    weather app

--- javascript/javascript3/week1/weather.js
+++ javascript/javascript3/week1/weather.js
@@ -39,11 +39,16 @@ function timeConverter(timestamp) {
   return time;
 }
 
-const getCity = () => {
-  const cityName = city.value;
+const getCity = (cityName) => {
+  //const cityName = city.value;
   if (cityName === "") {
     throw new Error("City name can't be empty");
   }
+
+  //SET LOCAL STORAGE
+
+  localStorage.setItem("location", cityName);
+
   displayWeather.innerHTML = "";
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=055e3200b3b25407a6c7935c9b2a6788`
@@ -81,49 +86,70 @@ const getCity = () => {
 };
 
 // Use my current position
-const getWeatherInCurrentLocation = () => {
+const getWeatherInCurrentLocation = (latitude, longitude) => {
   displayWeather.innerHTML = "";
 
+  //SET LOCAL STORAGE
+  const coordinates = {
+    latitude: latitude,
+    longitude: longitude,
+  };
+
+  localStorage.setItem("location", JSON.stringify(coordinates));
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=055e3200b3b25407a6c7935c9b2a6788`
+  )
+    .then((response) => response.json())
+    .then((weatherData) => {
+      cityDisplay.innerHTML = weatherData.name;
+      console.log(weatherData);
+      temperature.innerHTML = `${weatherData.main.temp} K`;
+      windSpeed.innerHTML = `Wind speed: ${weatherData.wind.speed}`;
+      cloudly.innerHTML = `Clouds: ${weatherData.clouds.all}`;
+      const dateSunrise = timeConverter(weatherData.sys.sunrise);
+      const dateSunset = timeConverter(weatherData.sys.sunset);
+      sunriseSunset.innerHTML = `Sunrise: ${dateSunrise}, Sunset: ${dateSunset}`;
+      icon.src = `http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+      celsiusButton.id = "celsius-button";
+      celsiusButton.innerHTML = "in Celsius";
+      displayWeather.appendChild(cityDisplay);
+      displayWeather.appendChild(celsiusButton);
+      displayWeather.appendChild(temperature);
+
+      displayWeather.appendChild(icon);
+      displayWeather.appendChild(windSpeed);
+      displayWeather.appendChild(cloudly);
+      displayWeather.appendChild(sunriseSunset);
+      //Additional feature: change to Celsius
+      document.addEventListener("click", function (e) {
+        if (e.target && e.target.id == "celsius-button") {
+          const numberTemp = parseInt(weatherData.main.temp);
+          const temperatureInCelsius = numberTemp - 273.15;
+          temperature.innerHTML = `${temperatureInCelsius.toFixed(2)} °C`;
+        }
+      });
+    });
+};
+button.addEventListener("click", function () {
+  getCity(city.value);
+});
+buttonWeatherCurrentLocation.addEventListener("click", function () {
   navigator.geolocation.getCurrentPosition((pos) => {
     latitude = pos.coords.latitude;
     longitude = pos.coords.longitude;
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=055e3200b3b25407a6c7935c9b2a6788`
-    )
-      .then((response) => response.json())
-      .then((weatherData) => {
-        cityDisplay.innerHTML = weatherData.name;
-        console.log(weatherData);
-        temperature.innerHTML = `${weatherData.main.temp} K`;
-        windSpeed.innerHTML = `Wind speed: ${weatherData.wind.speed}`;
-        cloudly.innerHTML = `Clouds: ${weatherData.clouds.all}`;
-        const dateSunrise = timeConverter(weatherData.sys.sunrise);
-        const dateSunset = timeConverter(weatherData.sys.sunset);
-        sunriseSunset.innerHTML = `Sunrise: ${dateSunrise}, Sunset: ${dateSunset}`;
-        icon.src = `http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
-        celsiusButton.id = "celsius-button";
-        celsiusButton.innerHTML = "in Celsius";
-        displayWeather.appendChild(cityDisplay);
-        displayWeather.appendChild(celsiusButton);
-        displayWeather.appendChild(temperature);
-
-        displayWeather.appendChild(icon);
-        displayWeather.appendChild(windSpeed);
-        displayWeather.appendChild(cloudly);
-        displayWeather.appendChild(sunriseSunset);
-        //Additional feature: change to Celsius
-        document.addEventListener("click", function (e) {
-          if (e.target && e.target.id == "celsius-button") {
-            const numberTemp = parseInt(weatherData.main.temp);
-            const temperatureInCelsius = numberTemp - 273.15;
-            temperature.innerHTML = `${temperatureInCelsius.toFixed(2)} °C`;
-          }
-        });
-      });
+    getWeatherInCurrentLocation(latitude, longitude);
   });
-};
-button.addEventListener("click", getCity);
-buttonWeatherCurrentLocation.addEventListener(
-  "click",
-  getWeatherInCurrentLocation
-);
+});
+//Check if local storage exists
+if (localStorage.getItem("location") != null) {
+  const storageValue = localStorage.getItem("location");
+  if (storageValue.includes("latitude")) {
+    const coordinatesFromStorage = JSON.parse(localStorage.getItem("location"));
+    latitude = coordinatesFromStorage.latitude;
+    longitude = coordinatesFromStorage.longitude;
+    getWeatherInCurrentLocation(latitude, longitude);
+  } else {
+    getCity(storageValue);
+  }
+}
