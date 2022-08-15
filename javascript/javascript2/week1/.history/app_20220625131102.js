7a4114517ee5cb75fa94f591ca9434cf6dda4b1e
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2022-08-15 10:30:07 +0200

    removed file

--- javascript/javascript2/week1/.history/app_20220625131102.js
+++
@@ -1,9 +0,0 @@
-const input = document.getElementById("input");
-const button = document.getElementById("button");
-const display = document.getElementById("display");
-function displayText() {
-  const text = input.value;
-  display.innerHTML = `${text} - `;
-}
-
-button.addEventListener("click", displayText);
--- javascript/javascript2/week2/.history/app_20220626155655.js
+++
@@ -1,6 +0,0 @@
-// DOUBLING OF NUMBERS
-
-let numbers = [1, 2, 3, 4];
-let newNumbers = [];
-
-console.log("The doubled numbers are", newNumbers); // [2, 6]
