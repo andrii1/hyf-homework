9af7c7af3fd6441d87335b8a8172e253d3c2e5ed
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2022-08-26 11:44:05 +0200

    more tasks done

--- databases/week1/index.sql
+++ databases/week1/index.sql
@@ -2,11 +2,19 @@
 SELECT COUNT(id) FROM task LIMIT 100;
 /*Find out how many tasks in the task table do not have a valid due date*/
 SELECT COUNT(id) FROM task WHERE `due_date` IS NULL LIMIT 100;
-Find all the tasks that are marked as done
-Find all the tasks that are not marked as done
-Get all the tasks, sorted with the most recently created first
-Get the single most recently created task
-Get the title and due date of all tasks where the title or description contains database
-Get the title and status (as text) of all tasks
+/* Find all the tasks that are marked as done */
+SELECT * FROM task JOIN status ON `status.id` = 'task.status_id' WHERE status.name = 'Done'
+
+/*Find all the tasks that are not marked as done*/
+
+SELECT * FROM task JOIN status ON status.id = task.status_id WHERE status.name != 'Done'
+/*Get all the tasks, sorted with the most recently created first*/
+SELECT * FROM task order by created desc
+/*Get the single most recently created task*/
+SELECT * FROM task order by created desc limit 1
+/*Get the title and due date of all tasks where the title or description contains database*/
+SELECT task.title, task.due_date FROM task WHERE description LIKE '%database%' OR title LIKE '%database%'
+/*Get the title and status (as text) of all tasks*/
+
 Get the name of each status, along with a count of how many tasks have that status
 Get the names of all statuses, sorted by the status with most tasks first*/
