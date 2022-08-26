b62d444ab21f7ba2bfa2dc47930adbffcd007f0a
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2022-08-26 12:09:34 +0200

    tasks finished

--- databases/week1/index.sql
+++ databases/week1/index.sql
@@ -15,6 +15,10 @@ SELECT * FROM task order by created desc limit 1
 /*Get the title and due date of all tasks where the title or description contains database*/
 SELECT task.title, task.due_date FROM task WHERE description LIKE '%database%' OR title LIKE '%database%'
 /*Get the title and status (as text) of all tasks*/
+SELECT task.title, status.name FROM task JOIN status ON status.id = task.status_id
+/*Get the name of each status, along with a count of how many tasks have that status*/
+SELECT status.name, COUNT(*) as number FROM task JOIN status ON status.id = task.status_id GROUP BY status.name
 
-Get the name of each status, along with a count of how many tasks have that status
-Get the names of all statuses, sorted by the status with most tasks first*/
+/*Get the names of all statuses, sorted by the status with most tasks first*/
+
+SELECT status.name, COUNT(*) as number FROM task JOIN status ON status.id = task.status_id GROUP BY status.name ORDER BY number DESC
