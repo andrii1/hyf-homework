fef204ffabab50a4788deb7b296ea5dd6e759c4a
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2022-09-02 21:43:45 +0200

    additional queries

--- databases/week3/additional_queries.sql
+++ databases/week3/additional_queries.sql
@@ -32,6 +32,40 @@ WHERE
   created_date BETWEEN '2021-08-10'
   AND '2022-10-01';
   /*Get only specific number of meals fx return only 5 meals*/
+SELECT
+  *
+FROM
+  meal
+LIMIT
+  5;
   /*Get the meals that have good reviews*/
+SELECT
+  *
+FROM
+  meal
+  JOIN review ON review.meal_id = meal.id
+WHERE
+  stars > 4
+LIMIT
+  100;
   /*Get reservations for a specific meal sorted by created_date*/
+SELECT
+  *
+FROM
+  reservation
+  JOIN meal ON reservation.meal_id = meal.id
+WHERE
+  meal.id = 1
+ORDER BY
+  meal.created_date ASC
   /*Sort all meals by average number of stars in the reviews*/
+SELECT
+  meal.title,
+  AVG(review.stars)
+FROM
+  meal
+  JOIN review ON review.meal_id = meal.id
+GROUP BY
+  meal.title
+ORDER by
+  AVG(review.stars) ASC
