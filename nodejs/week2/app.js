7f18953671de2c1649d0599a2547bce62a1b4af5
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2022-09-22 11:31:26 +0200

    documents/:id

--- nodejs/week2/app.js
+++ nodejs/week2/app.js
@@ -6,6 +6,7 @@ const documents = require("./documents.json");
 // Support parsing JSON requests
 app.use(express.json());
 
+//GET /search
 app.get("/search", (req, res) => {
   if (req.query.q) {
     const containsQuery = documents.filter(function (entry) {
@@ -15,6 +16,16 @@ app.get("/search", (req, res) => {
   } else {
     res.send(documents);
   }
+});
+
+//GET /documents/:id
+app.get("/documents/:id", async (req, res) => {
+  // TODO
+  const paramsInteger = parseInt(req.params.id);
+  const params = documents.filter(function (entry) {
+    return entry.id === paramsInteger;
+  });
+  res.send(params);
 });
 
 app.listen(port, () => {
