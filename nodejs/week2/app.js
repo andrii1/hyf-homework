fb2ac08ca436bdeca4a674face99b28ff5f566f4
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2022-09-22 12:29:21 +0200

    warmup

--- nodejs/week2/app.js
+++ nodejs/week2/app.js
@@ -19,13 +19,30 @@ app.get("/search", (req, res) => {
 });
 
 //GET /documents/:id
-app.get("/documents/:id", async (req, res) => {
+app.get("/documents/:id", (req, res) => {
   // TODO
   const paramsInteger = parseInt(req.params.id);
   const params = documents.filter(function (entry) {
     return entry.id === paramsInteger;
   });
   res.send(params);
+});
+
+//POST /search
+app.post("/search", (req, res) => {
+  const filterBody = req.body.fields;
+
+  if (req.query.q && req.body.fields) {
+    res.status(404).json({ error: "Bad request" });
+  } else if (filterBody) {
+    const filterByType = req.body.fields.type;
+    const filterResult = documents.filter(function (entry) {
+      return entry.type === filterByType;
+    });
+    res.send(filterResult);
+  } else {
+    res.status(404).json({ error: "Bad request, request body can't be empty" });
+  }
 });
 
 app.listen(port, () => {
