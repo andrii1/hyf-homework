299e22778ae28676facf864638c4edfdbcb13f65
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2022-10-13 14:57:07 +0200

    finished homework

---
+++ react/react1/week1/todolist/src/components/BlackBorder.js
@@ -0,0 +1,5 @@
+function BlackBorder({ children }) {
+  return <div className="black-border">{children}</div>;
+}
+
+export default BlackBorder;
--- react/react1/week1/todolist/src/components/ToDoList.js
+++ react/react1/week1/todolist/src/components/ToDoList.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import ItemRow from "./ItemRow";
+import BlackBorder from "./BlackBorder";
 import Header from "./Header";
 const API_URL =
   "https://gist.githubusercontent.com/benna100/391eee7a119b50bd2c5960ab51622532/raw";
@@ -11,6 +12,10 @@ function ToDoList() {
   const [todoitem, setTodoitem] = useState("");
   const [updateditem, setUpdateditem] = useState("");
   const [startDate, setStartDate] = useState(new Date());
+  const currentDate = new Date();
+  const currentDateHours = currentDate.setHours(0, 0, 0, 0);
+
+  //FETCH
   const fetchList = async () => {
     const response = await fetch(API_URL);
     const items = await response.json();
@@ -27,11 +32,12 @@ function ToDoList() {
   useEffect(() => {
     fetchList();
   }, []);
-  console.log(todoList);
+
+  //DELETE
   function deleteItem(deleteId) {
     setTodoList(todoList.filter(({ id }) => id !== deleteId));
   }
-
+  //EDIT
   function editItem(editId) {
     const newState = todoList.map((obj) => {
       if (obj.id === editId) {
@@ -42,17 +48,23 @@ function ToDoList() {
 
     setTodoList(newState);
   }
-
+  //UPDATE DESCRIPTION
   function updateItem(updateId) {
-    const newState = todoList.map((obj) => {
-      if (obj.id === updateId) {
-        return { ...obj, description: updateditem, editing: !obj.editing };
-      }
-      return obj;
-    });
+    if (updateditem.trim().length !== 0) {
+      const newState = todoList.map((obj) => {
+        if (obj.id === updateId) {
+          return { ...obj, description: updateditem, editing: !obj.editing };
+        }
+        return obj;
+      });
 
-    setTodoList(newState);
+      setTodoList(newState);
+      setUpdateditem("");
+    } else {
+      console.log("Updated item can't be empty!");
+    }
   }
+  //CHANGE STATUS
   function changeStatus(doneId) {
     const newState = todoList.map((obj) => {
       if (obj.id === doneId) {
@@ -64,19 +76,35 @@ function ToDoList() {
     setTodoList(newState);
   }
 
+  //ADD TODO ITEM
   function addTodo() {
-    setTodoList(
-      todoList.concat([
-        {
-          id: todoList.length + 1,
-          description: todoitem,
-          deadline: startDate.toISOString().slice(0, 10),
-          done: false,
-        },
-      ])
-    );
-    console.log(todoList);
+    //CHECK IF INPUT OR DATE IS EMPTY, IF DEADLINE BEFORE CURRENT DAY
+    let inputDate = new Date(startDate);
+    let inputDateHours = inputDate.setHours(0, 0, 0, 0);
+    if (
+      todoitem.trim().length !== 0 &&
+      startDate.length !== 0 &&
+      inputDateHours >= currentDateHours
+    ) {
+      setTodoList(
+        todoList.concat([
+          {
+            id: todoList.length + 1,
+            description: todoitem,
+            deadline: startDate.toISOString().slice(0, 10),
+            done: false,
+          },
+        ])
+      );
+    } else if (todoitem.trim().length === 0) {
+      console.log("input can't be empty!");
+    } else if (startDate.length === 0) {
+      console.log("date can't be empty!");
+    } else if (inputDateHours < currentDateHours) {
+      console.log("deadline is before current day!");
+    }
   }
+  //SET COUNTER
   useEffect(() => {
     const interval = setInterval(() => {
       setCount((c) => c + 1);
@@ -92,7 +120,7 @@ function ToDoList() {
         addItemText={(e) => setTodoitem(e.target.value)}
         value={todoitem}
         startDate={startDate}
-        datepicker={(date) => setStartDate(date)}
+        datepicker={(date) => setStartDate(date ? date : "")}
       >
         <h1>To do list</h1>
         <p>You have used {count} seconds on this website</p>
@@ -101,19 +129,21 @@ function ToDoList() {
         <p>No items</p>
       ) : (
         todoList.map(({ description, id, deadline, done, editing }) => (
-          <ItemRow
-            description={description}
-            key={id}
-            onDelete={() => deleteItem(id)}
-            onEdit={() => editItem(id)}
-            onUpdate={() => updateItem(id)}
-            changeStatus={() => changeStatus(id)}
-            className={done ? "striked" : ""}
-            deadline={deadline}
-            value={updateditem}
-            editing={editing ? true : false}
-            updateItemText={(e) => setUpdateditem(e.target.value)}
-          />
+          <BlackBorder>
+            <ItemRow
+              description={description}
+              key={id}
+              onDelete={() => deleteItem(id)}
+              onEdit={() => editItem(id)}
+              onUpdate={() => updateItem(id)}
+              changeStatus={() => changeStatus(id)}
+              className={done ? "striked" : ""}
+              deadline={deadline}
+              value={updateditem}
+              editing={editing ? true : false}
+              updateItemText={(e) => setUpdateditem(e.target.value)}
+            />
+          </BlackBorder>
         ))
       )}
     </>
